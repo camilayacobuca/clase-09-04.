e5396926ffa1fee6e13c369e9de9103338735098
camilayacobuca <camilayacob@uca.edu.ar>
2026-04-09 19:27:21 -0300

    Add files via upload

---
+++ Untitled6 (1).py
@@ -0,0 +1,9 @@
+nombre="camila"
+'''
+contador=0
+for letra in nombre:
+    if letra != " ":
+        contador=contador+1
+        '''
+print(len(nombre))
+    
---
+++ whileUntitled12.py
@@ -0,0 +1,7 @@
+nombre "camila"
+contador = 0
+1=0
+while i < len(nombre):
+    contador=contador+1
+    i=i+1
+print("la cantidad de letras son:",contador)
